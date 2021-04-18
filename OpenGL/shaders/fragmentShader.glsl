a280fec4972a6857d6557b12c7de4a3c6c779229
rohitrtk <rohit_rtk@hotmail.com>
2021-04-18 02:05:30 -0400

    Finished Exercise2-3

--- OpenGL/shaders/fragmentShader.glsl
+++ OpenGL/shaders/fragmentShader.glsl
@@ -2,9 +2,10 @@
 
 out vec4 FragColor;
 
-in vec3 ourColor;
+//in vec3 ourColor;
+in vec3 ourPosition;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0f);
+    FragColor = vec4(ourPosition, 1.0);//vec4(ourColor, 1.0f);
 }
